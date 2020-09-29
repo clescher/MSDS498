6e890b16df85f73501a56d8a99ad8750f7d44575
clescher <71951400+clescher@users.noreply.github.com>
2020-09-29 17:06:10 -0500

    Update convert the date.R

--- convert the date.R
+++ convert the date.R
@@ -8,10 +8,18 @@ data <- read.csv("smallerdata.csv")
 
 dateconvert <- function(x){
 
+  #grabs the first 3 digits which should be the month
   month<- substr(x, 1, 3)
+  #grabs the last 4 digits which should be the year
   year <- substr(x,5,8)
+  #pastes it together into a string with the 1st of the month
   thedate <- paste(month,"-01-",year, sep="")
+  #returns as a date
   return(as.Date(thedate, format = "%b-%d-%y"))
 }
 
 data$last_credit_pull_d2 <- dateconvert(data$last_credit_pull_d)
+
+#proof that all NA's aren't my fault rather the data is missing :)
+data1 <- data[is.na(data$last_credit_pull_d2),]
+data1$last_credit_pull_d
